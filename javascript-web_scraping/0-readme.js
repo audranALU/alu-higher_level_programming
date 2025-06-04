ea12113de8d80f0d14240b05df7e453d8ba52eb5
root <a.ndayiseng@alustudent.com>
2025-06-04 09:27:00 +0000

    ws

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,4 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+fs.readFile(process.argv[2], 'utf-8', (err, data) => console.log(err || data));
 
